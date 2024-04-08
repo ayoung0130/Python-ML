4867c1543323d594f251bb1490d230c06b7e7aa6
ayoung0130 <149597700+ayoung0130@users.noreply.github.com>
2024-04-08 20:01:12 +0900

    0408

--- LSTM-Practice/create_dataset.py
+++ LSTM-Practice/create_dataset.py
@@ -112,7 +112,7 @@ print("data[20]\n", data[20])
 # 시퀀스 데이터 저장
 full_seq_data = [data[seq:seq + seq_length] for seq in range(len(data) - seq_length)]
 full_seq_data = np.array(full_seq_data)
-# np.save(os.path.join(save_path, f'seq_{action}_{created_time}'), full_seq_data)
+np.save(os.path.join(save_path, f'seq_{action}_{created_time}'), full_seq_data)
 print("seq data shape:", action, full_seq_data.shape)
 
 # 사용된 함수, 자원 해제
